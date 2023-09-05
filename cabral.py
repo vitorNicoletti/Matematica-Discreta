99bff4df69b1f496ab68efe4fa4e05e7e035e2bb
Vitor Nicoletti <128414008+vitorNicoletti@users.noreply.github.com>
2023-09-05 10:28:09 -0300

    Update cabral.py

--- cabral.py
+++ cabral.py
@@ -1,28 +1,35 @@
-arquivo = open("arquivo2.txt", "r")
-texto = arquivo.readlines()
+def main():
+  file = open("input2.txt", "r")
+  line = file.readlines()
 
-quantidadeOperacoes = int(texto[0].replace("\n", ""))
+  quantidadeOperacoes = int(line[0].replace("\n", ""))
 
-for i in range(1, ((quantidadeOperacoes * 3) + 1), 3):
-  operacao = texto[i].replace("\n", "").replace(",", "")
-  conjuntoA = set(texto[i + 1].replace("\n", "").split(","))
-  conjuntoB = set(texto[i + 2].replace("\n", "").split(","))
-  if operacao == "I":
-    print(
-        f"\nInterseção: Conjunto 1 {conjuntoA}, Conjunto 2 {conjuntoB}. Resultado: ",
-        conjuntoA.intersection(conjuntoB))
-  elif operacao == "U":
-    print(
-        f"\nUnião: Conjunto 1 {conjuntoA}, Conjunto 2 {conjuntoB}. Resultado: ",
-        conjuntoA.union(conjuntoB))
-  elif operacao == "D":
-    print(
-        f"\nDiferença: Conjunto 1 {conjuntoA}, Conjunto 2 {conjuntoB}. Resultado: ",
-        conjuntoA.difference(conjuntoB))
-  elif operacao == "C":
-    print(
-        f"\nProduto Cartesiano: Conjunto 1 {conjuntoA}, Conjunto 2 {conjuntoB}. Resultado: ",
-        end="")
-    for i in conjuntoA:
-      for j in conjuntoB:
-        print(f"({i},{j}), ", end="")
+  for i in range(1, ((quantidadeOperacoes * 3) + 1), 3):
+
+    operacao = line[i].replace("\n", "").replace(",", "")
+    conjA = set(line[i + 1].replace("\n", "").split(","))
+    conjB = set(line[i + 2].replace("\n", "").split(","))
+
+    if operacao == "I":
+      print(
+          f"\nInterseção: Conjunto 1 {conjA}, Conjunto 2 {conjB}. Resultado: ",
+          conjA.intersection_update(conjB))
+    elif operacao == "U":
+      print(f"\nUnião: Conjunto 1 {conjA}, Conjunto 2 {conjB}. Resultado: ",
+            conjA.union(conjB))
+
+    elif operacao == "D":
+      print(
+          f"\nDiferença: Conjunto 1 {conjA}, Conjunto 2 {conjB}. Resultado: ",
+          conjA.difference(conjB))
+    elif operacao == "C":
+      print(
+          f"\nProduto Cartesiano: Conjunto 1 {conjA}, Conjunto 2 {conjB}. Resultado: ",
+          end="")
+      for i in conjA:
+        for j in conjB:
+          print(f"({i},{j}), ", end="")
+
+
+if __name__ == "__main__":
+  main()
