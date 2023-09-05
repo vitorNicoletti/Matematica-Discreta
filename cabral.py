0fde6c006cc3df64fd03560f8728a6379a58d88f
Contin <124305689+EduContin@users.noreply.github.com>
2023-09-05 10:30:35 -0300

    Format Update

--- cabral.py
+++ cabral.py
@@ -1,35 +1,27 @@
 def main():
-  file = open("input2.txt", "r")
-  line = file.readlines()
+    file = open("input.txt", "r")
+    line = file.readlines()
 
-  quantidadeOperacoes = int(line[0].replace("\n", ""))
+    quantidadeOperacoes = int(line[0].replace("\n", ""))
 
-  for i in range(1, ((quantidadeOperacoes * 3) + 1), 3):
+    for i in range(1, ((quantidadeOperacoes * 3) + 1), 3):
 
-    operacao = line[i].replace("\n", "").replace(",", "")
-    conjA = set(line[i + 1].replace("\n", "").split(","))
-    conjB = set(line[i + 2].replace("\n", "").split(","))
+        operacao = line[i].replace("\n", "").replace(",", "")
+        conjA = set(line[i + 1].replace("\n", "").replace(" ", "").split(","))
+        conjB = set(line[i + 2].replace("\n", "").replace(" ", "").split(","))
 
-    if operacao == "I":
-      print(
-          f"\nInterseção: Conjunto 1 {conjA}, Conjunto 2 {conjB}. Resultado: ",
-          conjA.intersection_update(conjB))
-    elif operacao == "U":
-      print(f"\nUnião: Conjunto 1 {conjA}, Conjunto 2 {conjB}. Resultado: ",
-            conjA.union(conjB))
-
-    elif operacao == "D":
-      print(
-          f"\nDiferença: Conjunto 1 {conjA}, Conjunto 2 {conjB}. Resultado: ",
-          conjA.difference(conjB))
-    elif operacao == "C":
-      print(
-          f"\nProduto Cartesiano: Conjunto 1 {conjA}, Conjunto 2 {conjB}. Resultado: ",
-          end="")
-      for i in conjA:
-        for j in conjB:
-          print(f"({i},{j}), ", end="")
-
+        if operacao == "I":
+            print(f"\nInterseção: conjunto 1 {conjA}, conjunto 2 {conjB}. Resultado: ", conjA.intersection_update(conjB))
+        elif operacao == "U":
+            print(f"\nUnião: conjunto 1 {conjA}, conjunto 2 {conjB}. Resultado: ", conjA.union(conjB))
+        elif operacao == "D":
+            print(f"\nDiferença: conjunto 1 {conjA}, conjunto 2 {conjB}. Resultado: ", conjA.difference(conjB))
+        elif operacao == "C":
+            print(f"\nProduto Cartesiano: conjunto 1 {conjA}, conjunto 2 {conjB}. Resultado: ", end="")
+            
+            for i in conjA:
+                for j in conjB:
+                    print(f"({i.replace(' ', '')},{j.replace(' ', '')}), ", end="")
 
 if __name__ == "__main__":
-  main()
+    main()
